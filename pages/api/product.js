06fa3bf8e47be3369ae0218f05b9810eb269ebd3
Salih <salihsert18@gmail.com>
2020-01-31 19:19:51 +0100

    add flex

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,39 +1,35 @@
+import Product from '../../models/Product';
+import Cart from '../../models/Cart';
 import connectDb from '../../utils/connectDb';
-import Product from './../../models/Product';
 
 connectDb();
 
-const handleGetRequest = async (req, res) => {
+export default async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      await handleGetRequest(req, res);
+      break;
+    case 'POST':
+      await handlePostRequest(req, res);
+      break;
+    case 'DELETE':
+      await handleDeleteRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+      break;
+  }
+};
+
+async function handleGetRequest(req, res) {
   const { _id } = req.query;
   const product = await Product.findOne({ _id });
   res.status(200).json(product);
-};
-
-
-const handleDeleteRequest = (req, res) => {
-  const { _id } = req.query;
-
-  try {
-    // 1) Delete product by id 
-    await Product.findOneAndDelete({ _id });
-
-    // 2) Remove from all carts, referenced as "product"
-    await Cart.updateMany(
-      { "products.product": _id },
-      { $pull: { products: { product: _id } } }
-    )
-    res.status(204).json({});
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error deleting product") 
-  }
 }
 
-
-const handlePostRequest = async (req, res) => {
+async function handlePostRequest(req, res) {
+  const { name, price, description, mediaUrl } = req.body;
   try {
-    const { name, price, description, mediaUrl } = req.body;
     if (!name || !price || !description || !mediaUrl) {
       return res.status(422).send('Product missing one or more fields');
     }
@@ -43,26 +39,25 @@ const handlePostRequest = async (req, res) => {
       description,
       mediaUrl,
     }).save();
-
     res.status(201).json(product);
   } catch (error) {
-    res.status(500).send('Server error in creation product');
     console.error(error);
+    res.status(500).send('Server error in creating product');
   }
-};
+}
 
-export default async (req, res) => {
-  switch (req.method) {
-    case 'GET':
-      await handleGetRequest(req, res);
-      break;
-    case 'DELETE':
-      await handleDeleteRequest(req, res);
-      break;
-    case 'POST':
-      await handlePostRequest(req, res);
-      break;
-    default:
-      res.status(405).send(`Method ${req.method} not allowed`);
+async function handleDeleteRequest(req, res) {
+  const { _id } = req.query;
+
+  try {
+    // 1) Delete product by id
+    await Product.findOneAndDelete({ _id });
+
+    // 2) Remove from all carts, referenced as "product"
+    await Cart.updateMany({ 'products.product': _id }, { $pull: { products: { product: _id } } });
+    res.status(204).json({});
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error deleting product');
   }
-};
+}
