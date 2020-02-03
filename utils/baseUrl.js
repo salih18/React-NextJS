64d83c96b51877f1a29a04df0c9a8e4774afb98b
Salih <salihsert18@gmail.com>
2020-02-03 14:02:02 +0100

    heroku

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://salih-react-shop.salihsert18.now.sh'
+    ? 'https://shopping-app-nextjs.herokuapp.com'
     : 'http://localhost:3000';
 
 export default baseUrl;
